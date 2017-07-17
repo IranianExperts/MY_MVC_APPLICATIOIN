99c23d30184c4e75c9bc8e47e2026ec7bf1f5b03
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2017-07-17 12:28:06 +0430

    Adding some models and create RolesController and its Views.

--- MY_MVC_APPLICATION/Controllers/HomeController.cs
+++ MY_MVC_APPLICATION/Controllers/HomeController.cs
@@ -3,13 +3,47 @@ using System.Data.Entity;
 
 namespace MY_MVC_APPLICATION.Controllers
 {
-	public class HomeController : Infrastructure.BaseController
+	public partial class HomeController : Infrastructure.BaseController
 	{
 		public HomeController() : base()
 		{
 		}
 
-		public System.Web.Mvc.ViewResult Index()
+		public virtual System.Web.Mvc.ViewResult Index()
+		{
+			//Models.DatabaseContext oDatabaseContext = null;
+
+			//try
+			//{
+			//	oDatabaseContext =
+			//		new Models.DatabaseContext();
+
+			//	var varRoles =
+			//		oDatabaseContext.Roles
+			//		.OrderBy(current => current.Name)
+			//		.ToList()
+			//		;
+
+			//	int intRoleCount = varRoles.Count;
+			//}
+			//catch (System.Exception ex)
+			//{
+			//	// خطا را لاگ کرده 
+			//	// و پیام مناسبی به کاربر می‌دهیم
+			//}
+			//finally
+			//{
+			//	if (oDatabaseContext != null)
+			//	{
+			//		oDatabaseContext.Dispose();
+			//		oDatabaseContext = null;
+			//	}
+			//}
+
+			return (View());
+		}
+
+		public virtual System.Web.Mvc.ViewResult About()
 		{
 			return (View());
 		}
